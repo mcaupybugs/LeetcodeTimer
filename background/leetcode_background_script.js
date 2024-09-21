5f0e9144e48387130d15c9419077c439791f3ca9
Vishal Yadav <vishalydv.me@gmail.com>
2024-09-21 19:26:58 +0530

    Adding the latest changes

--- background/leetcode_background_script.js
+++ background/leetcode_background_script.js
@@ -1,23 +1,25 @@
 var dataArray = [];
 var setArray = new Set();
 chrome.runtime.onStartup.addListener(function () {
-    chrome.storage.local.get(['leetcode_timer_file']).then((result) => {
-        var file = result.leetcode_timer_file;
-        if (!file) {
-            dataArray = [];
-            setArray = new Set()
-            chrome.storage.local.set({ "leetcode_timer_file": dataArray }).then(() => {
-                console.log("The value is set")
-            })
-        }
-        dataArray = file ? file : [];
-        setArray = file ? new Set(file.map(item => item.questionTitle)) : new Set();
-        console.log(setArray)
-        console.log(dataArray)
-    })
+    ensureFilePresent();
 })
+
 chrome.tabs.onActivated.addListener((tab) => {
+    ensureFilePresent();
     chrome.tabs.get(tab.tabId, function (tab) {
+        var pageTitle = tab.title
+        var pageUrl = tab.url
+        console.log(dataArray)
+        console.log(setArray)
+        if (checkLeetcodePage(pageUrl) && !setArray.has(pageTitle)) {
+            dataArray.push(new CSVDataModel(pageTitle, pageUrl, "10", new Date().toDateString()))
+            chrome.storage.local.set({ "leetcode_timer_file": dataArray })
+        }
+    })
+});
+
+chrome.tabs.onUpdated.addListener((tabId) => {
+    chrome.tabs.get(tabId, function (tab) {
         var pageTitle = tab.title
         var pageUrl = tab.url
         if (checkLeetcodePage(pageUrl) && !setArray.has(pageTitle)) {
@@ -35,6 +37,25 @@ function checkLeetcodePage(url) {
     return false;
 }
 
+function handleFileNotFound(result) {
+    var file = result.leetcode_timer_file;
+    if (!file) {
+        dataArray = [];
+        setArray = new Set()
+        chrome.storage.local.set({ "leetcode_timer_file": dataArray }).then(() => {
+            console.log("Initial Doc Value set")
+        })
+    }
+}
+
+function ensureFilePresent() {
+    chrome.storage.local.get(['leetcode_timer_file']).then((result) => {
+        handleFileNotFound(result)
+        var file = result.leetcode_timer_file;
+        dataArray = file ? file : [];
+        setArray = file ? new Set(file.map(item => item.questionTitle)) : new Set();
+    })
+}
 // ------------------------------------------- Data model -----------------
 
 function CSVDataModel(questionTitle, questionUrl, timeTaken, date) {
